f32727bfa9092e2b479e676f9a6b1ca1e4d7e704
thegx <village.cowherd@gmail.com>
2022-12-26 14:10:17 +0800

    update lsp server config

--- lua/lsp/init.lua
+++ lua/lsp/init.lua
@@ -1,6 +1,33 @@
 require("mason").setup()
-require("mason-lspconfig").setup({
-  -- 自动安装 Language Servers
-  automatic_installation = true,
-  ensure_installed = { "rust_analyzer" },
+require("mason-lspconfig").setup_handlers({
+  ["sumneko_lua"] = function() 
+    require("lsp.config.lua")
+  end,
+  ["bashls"] = function()
+    require("lsp.config.bash")
+  end,
+  ["pyright"] = function()
+    require("lsp.config.pyright")
+  end,
+  ["html"] = function()
+    require("lsp.config.html")
+  end,
+  ["cssls"] = function()
+    require("lsp.config.css")
+  end,
+  ["emmet_ls"] = function()
+    require("lsp.config.emmet")
+  end,
+  ["jsonls"] = function()
+    require("lsp.config.json")
+  end,
+  ["tsserver"] = function()
+    require("lsp.config.ts")
+  end,
+  ["rust_analyzer"] = function()
+    require("lsp.config.rust")
+  end,
+  ["yamlls"] = function()
+    require("lsp.config.yamlls")
+  end,
 })

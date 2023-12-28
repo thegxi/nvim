66fec38c3c976f28191287bbe6787573c89c6e85
xi <village.cowherd@gmail.com>
2023-12-28 22:40:24 +0800

    lambdalisue/suda.vim keymap

---
+++ lua/plugins/keymap/editor.lua
@@ -0,0 +1,12 @@
+local util = require('plugins.util')
+local map_cr = util.map_cr
+local map_cu = util.map_cu
+local map_cmd = util.map_cmd
+local map_callback = util.map_callback
+
+local plugin_map = {
+  -- Plugin suda.vim
+	["n|<C-S-s>"] = map_cu("SudaWrite"):with_silent():with_noremap():with_desc("editn: Save file using sudo"),
+}
+
+util.nvim_load_mapping(plugin_map)

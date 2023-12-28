b0f508731f2aecd8c54c4a2e88fa5a07b0cbfcf5
xi <village.cowherd@gmail.com>
2023-12-28 16:21:22 +0800

    install&config nvim-cmp

--- lua/plugins/completion.lua
+++ lua/plugins/completion.lua
@@ -65,5 +65,31 @@ return {
       'nvim-tree/nvim-web-devicons'     -- optional
     },
     config = require('plugins.completion.lspsaga')
+  },
+  {
+    'hrsh7th/nvim-cmp',
+    lazy = true,
+    ft = vim.g.fts,
+    dependencies = {
+      'hrsh7th/cmp-nvim-lsp',
+      'hrsh7th/cmp-nvim-lua',
+      'hrsh7th/cmp-buffer',
+      'hrsh7th/cmp-path',
+      'hrsh7th/cmp-cmdline',
+      'hrsh7th/nvim-cmp',
+      {
+        "L3MON4D3/LuaSnip",
+        -- follow latest release.
+        version = "v2.*", -- Replace <CurrentMajor> by the latest released major (first number of latest release)
+        -- install jsregexp (optional!).
+        build = "make install_jsregexp",
+        dependencies = { "rafamadriz/friendly-snippets" },
+        config = function()
+          -- load snippets from path/of/your/nvim/config/my-cool-snippets
+          require("luasnip.loaders.from_snipmate").lazy_load({ paths = { "~/.config/nvim/snippets" } })
+        end
+      }
+    },
+    config = require('plugins.completion.cmp')
   }
 }
---
+++ lua/plugins/completion/cmp.lua
@@ -0,0 +1,95 @@
+return function()
+  require('cmp').setup({
+    experimental = {
+      ghost_text = true
+    },
+    formatting = {
+      fields = { 'abbr', 'menu', 'kind' },
+      format = function(entry, vim_item)
+      local lspkind_icons = vim.tbl_deep_extend("force", icons.kind, icons.type, icons.cmp)
+				-- load lspkind icons
+				vim_item.kind =
+					string.format(" %s  %s", lspkind_icons[vim_item.kind] or icons.cmp.undefined, vim_item.kind or "")
+
+				vim_item.menu = setmetatable({
+					cmp_tabnine = "[TN]",
+					copilot = "[CPLT]",
+					buffer = "[BUF]",
+					orgmode = "[ORG]",
+					nvim_lsp = "[LSP]",
+					nvim_lua = "[LUA]",
+					path = "[PATH]",
+					tmux = "[TMUX]",
+					treesitter = "[TS]",
+					latex_symbols = "[LTEX]",
+					luasnip = "[SNIP]",
+					spell = "[SPELL]",
+				}, {
+					__index = function()
+						return "[BTN]" -- builtin/unknown source names
+					end,
+				})[entry.source.name]
+
+				local label = vim_item.abbr
+				local truncated_label = vim.fn.strcharpart(label, 0, 80)
+				if truncated_label ~= label then
+					vim_item.abbr = truncated_label .. "..."
+				end
+
+				return vim_item
+			end,
+    },
+    mapping = cmp.mapping.preset.insert({
+      ['<Tab>'] = cmp.mapping(function(fallback)
+        if cmp.visible() then
+          cmp.select_next_item()
+        elseif require('luasnip').expand_or_locally_jumpable() then
+          vim.fn.feedkeys(t('<Plug>luasnip-expand-or-jump'))
+        else
+          fallback()
+        end
+      end, { 'i', 's' }),
+      ['<S-Tab>'] = cmp.mapping(function(fallback)
+        if cmp.visible() then
+          cmp.select_prev_item()
+        elseif require('luasnip').jumpable(-1) then
+          vim.fn.feedkeys(t('<Plug>luasnip-jump-prev'), '')
+        else
+          fallback()
+        end
+      end, { 'i', 's' }),
+      ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+    }),
+    snippet = {
+      expand = function(args)
+        require('luasnip').lsp_expand(args.body)
+      end
+    },
+    sources = cmp.config.sources({
+      { name = 'nvim_lsp' },
+      { name = 'nvim_lua' },
+      -- { name = 'vsnip' }, -- For vsnip users.
+      { name = 'luasnip' }, -- For luasnip users.
+      -- { name = 'ultisnips' }, -- For ultisnips users.
+      -- { name = 'snippy' }, -- For snippy users.
+      }, 
+      {
+      { name = 'buffer' },
+      { name = 'path' },
+    }),
+    window = {
+      completion = {
+        winhighlight = 'Normal:Pmenu,FlostBorder:Pmenu,Search:None',
+        col_offset = -3,
+        side_padding = 0,
+        border = 'rounded',
+        scrollbar = true
+      },
+      documentation = {
+        winhighlight = 'Normal:Pmenu,FlostBorder:Pmenu,Search:None',
+        border = 'rounded',
+        scrollbar = true
+      }
+    }
+  })
+end

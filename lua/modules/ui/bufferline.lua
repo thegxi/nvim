d6d9a6c9f2b20d49b750fc4a510c65afd45a59dd
xi <village.cowherd@gmail.com>
2024-08-10 23:24:17 +0800

    update: bufferline options

--- lua/modules/ui/bufferline.lua
+++ lua/modules/ui/bufferline.lua
@@ -1,80 +1,71 @@
 return function()
-	local bufferline = require("bufferline")
 	local icons = require("utils.icons")
-	function  bufremove(buf)
-		buf = buf or 0
-		buf = buf == 0 and vim.api.nvim_get_current_buf() or buf
-
-		if vim.bo.modified then
-			local choice = vim.fn.confirm(("Save changes to %q?"):format(vim.fn.bufname()), "&Yes\n&No\n&Cancel")
-			if choice == 0 or choice == 3 then -- 0 for <Esc>/<C-c> and 3 for Cancel
-				return
-			end
-			if choice == 1 then -- Yes
-				vim.cmd.write()
-			end
-		end
-
-		for _, win in ipairs(vim.fn.win_findbuf(buf)) do
-			vim.api.nvim_win_call(win, function()
-				if not vim.api.nvim_win_is_valid(win) or vim.api.nvim_win_get_buf(win) ~= buf then
-					return
-				end
-				-- Try using alternate buffer
-				local alt = vim.fn.bufnr("#")
-				if alt ~= buf and vim.fn.buflisted(alt) == 1 then
-					vim.api.nvim_win_set_buf(win, alt)
-					return
-				end
-
-				-- Try using previous buffer
-				local has_previous = pcall(vim.cmd, "bprevious")
-				if has_previous and buf ~= vim.api.nvim_win_get_buf(win) then
-					return
-				end
-
-				-- Create new listed buffer
-				local new_buf = vim.api.nvim_create_buf(true, false)
-				vim.api.nvim_win_set_buf(win, new_buf)
-			end)
-		end
-		if vim.api.nvim_buf_is_valid(buf) then
-			pcall(vim.cmd, "bdelete! " .. buf)
-		end
-	end
-	options = {
-		-- stylua: ignore
-		close_command = function(n) bufremove(n) end,
-		-- stylua: ignore
-		right_mouse_command = function(n) bufremove(n) end,
-		diagnostics = "nvim_lsp",
-		always_show_bufferline = false,
-		diagnostics_indicator = function(_, _, diag)
-			local diag_icons = icons.diagnostics
-			local ret = (diag.error and diag_icons.Error .. diag.error .. " " or "")
-				.. (diag.warning and diag_icons.Warn .. diag.warning or "")
-			return vim.trim(ret)
-		end,
-		offsets = {
-			{
-				filetype = "neo-tree",
-				text = "Neo-tree",
-				highlight = "Directory",
-				text_align = "left",
+	local opts = {
+		options = {
+			number = nil,
+			close_command = "BufDel! %d",
+			right_mouse_command = "BufDel! %d",
+			modified_icon = icons.ui.Modified,
+			buffer_close_icon = icons.ui.Close,
+			left_trunc_marker = icons.ui.Left,
+			right_trunc_marker = icons.ui.Right,
+			max_name_length = 20,
+			max_prefix_length = 13,
+			tab_size = 20,
+			color_icons = true,
+			show_buffer_icons = true,
+			show_buffer_close_icons = true,
+			show_close_icon = true,
+			show_tab_indicators = true,
+			enforce_regular_tabs = false,
+			persist_buffer_sort = true,
+			always_show_bufferline = true,
+			separator_style = "thin",
+			diagnostics = "nvim_lsp",
+			diagnostics_indicator = function(count)
+				return "(" .. count .. ")"
+			end,
+			offsets = {
+				{
+					filetype = "NvimTree",
+					text = "File Explorer",
+					text_align = "center",
+					padding = 0,
+				},
+				{
+					filetype = "aerial",
+					text = "Symbol Outline",
+					text_align = "center",
+					padding = 0,
+				},
 			},
 		},
-		---@param opts bufferline.IconFetcherOpts
-		get_element_icon = function(opts)
-			return icons.ft[opts.filetype]
-		end,
+		-- Change bufferline's highlights here! See `:h bufferline-highlights` for detailed explanation.
+		-- Note: If you use catppuccin then modify the colors below!
+		highlights = {},
 	}
-	bufferline.setup(options)
-	-- Fix bufferline when restoring a session
-	vim.api.nvim_create_autocmd({ "BufAdd", "BufDelete" }, {
-		callback = function()
-			vim.schedule(function()
-			  pcall(nvim_bufferline)
-			end)
-		end,
-	})
+	if vim.g.colors_name:find("catppuccin") then
+		local cp = require("modules.utils").get_palette() -- Get the palette.
+
+		local catppuccin_hl_overwrite = {
+			highlights = require("catppuccin.groups.integrations.bufferline").get({
+				styles = { "italic", "bold" },
+				custom = {
+					all = {
+						-- Hint
+						hint = { fg = cp.rosewater },
+						hint_visible = { fg = cp.rosewater },
+						hint_selected = { fg = cp.rosewater },
+						hint_diagnostic = { fg = cp.rosewater },
+						hint_diagnostic_visible = { fg = cp.rosewater },
+						hint_diagnostic_selected = { fg = cp.rosewater },
+					},
+				},
+			}),
+		}
+
+		opts = vim.tbl_deep_extend("force", opts, catppuccin_hl_overwrite)
+	end
+
+	require("bufferline").setup(opts)
 end

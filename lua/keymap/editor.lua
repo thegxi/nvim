d7b254032bd32426a9b333ddc4cb81c0463f64b7
xi <village.cowherd@gmail.com>
2024-08-11 14:00:36 +0800

    fix: grug-far keymap

--- lua/keymap/editor.lua
+++ lua/keymap/editor.lua
@@ -4,7 +4,7 @@ local plug_map = {
 	-- Plugin: neo-tree.nvim
 	["n|<leader>e"] = bind.map_cr("Neotree"):with_noremap():with_silent():with_desc("file-tree: Open tree"),
 	-- grug-far
-	["n|<leader>gw"] = bind.map_cr("lua require('grug-far').grug_far({ prefills = { search = vim.fn.expand('<cword>') } })"):with_noremap():with_silent():with_desc("search: current word under the cursor")
+	["n|<leader>gw"] = bind.map_cr("lua require('grug-far').grug_far({ prefills = { search = vim.fn.expand('<cword>') } })"):with_noremap():with_silent():with_desc("search: current word under the cursor"),
 	["n|<leader>ge"] = bind.map_cr("lua require('grug-far').grug_far({ engine = 'astgrep' })"):with_noremap():with_silent():with_desc("grug-far: use ast-grep engine"),
 	["n|<leader>gb"] = bind.map_cr("lua require('grug-far').grug_far({ transient = true })"):with_noremap():with_silent():with_desc("grug-far: as a transient buffer which is both unlisted and fully deletes itself when not in use"),
 	["n|<leader>gs"] = bind.map_cr("lua require('grug-far').grug_far({ prefills = { paths = vim.fn.expand('%') } })"):with_noremap():with_silent():with_desc("grug-far: limiting search/replace to current file"),

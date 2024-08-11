4f25bbff6bf23449e6252d4feb823be4754a7fc6
xi <village.cowherd@gmail.com>
2024-08-11 23:43:39 +0800

    feat: A completion engine plugin for neovim written in Lua. Completion sources are installed from external repositories and "sourced".

---
+++ lua/modules/completion/package.lua
@@ -0,0 +1,31 @@
+return {
+  -- nvim-cmp
+  {
+    "hrsh7th/nvim-cmp",
+    version = false, -- last release is way too old
+    event = "InsertEnter",
+    dependencies = {
+      "hrsh7th/cmp-nvim-lsp",
+      "hrsh7th/cmp-buffer",
+      "hrsh7th/cmp-path",
+      "hrsh7th/cmp-buffer",
+      {
+        "garymjr/nvim-snippets",
+        opts = {
+          friendly_snippets = true
+        },
+        dependencies = { "rafamadriz/friendly-snippets" }
+      }
+    },
+    -- Not all LSP servers add brackets when completing a function.
+    -- To better deal with this, LazyVim adds a custom option to cmp,
+    -- that you can configure. For example:
+    --
+    -- ```lua
+    -- opts = {
+    --   auto_brackets = { "python" }
+    -- }
+    -- ```
+    config = require("modules.completion.cmp")
+	}
+}

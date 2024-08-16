350f8aa0020aa216a39d88320583058beee12283
xi <village.cowherd@gmail.com>
2024-08-16 22:55:24 +0800

    update: format config

--- lua/modules/tool/conform.lua
+++ lua/modules/tool/conform.lua
@@ -1,92 +1,112 @@
 return function()
-  local conform = require("conform")
-
-  local formatters = {
-    -- Python
-    {
-      cmd = { "black", "-" }, -- 使用 black 的 stdin/stdout 模式
-      filetypes = { "python" },
+  local options = {
+    -- Map of filetype to formatters
+    formatters_by_ft = {
+      fish = { "fish_indent" },
+      go = { "gofumpt", "goimports" },
+      graphql = { { "prettierd", "prettier" } },
+      handlebars = { { "prettierd", "prettier" } },
+      html = { { "prettierd", "prettier" } },
+      javascript = { { "prettierd", "prettier" } },
+      javascriptreact = { { "prettierd", "prettier" } },
+      json = { { "prettierd", "prettier" } },
+      jsonc = { { "prettierd", "prettier" } },
+      less = { { "prettierd", "prettier" } },
+      lua = { "stylua", "stylua" },
+      markdown = { { "prettierd", "prettier" } },
+      ["markdown.mdx"] = { { "prettierd", "prettier" } },
+      protobuf = { "buf" },
+      python = { "isort", "black" },
+      rust = { "rustfmt" },
+      scss = { { "prettierd", "prettier" } },
+      sh = { "shfmt", "shfmt" },
+      terraform = { "terraform_fmt" },
+      ["terraform-vars"] = { "terraform_fmt" },
+      tf = { "terraform_fmt" },
+      typescript = { { "prettierd", "prettier" } },
+      typescriptreact = { { "prettierd", "prettier" } },
+      vue = { { "prettierd", "prettier" } },
+      yaml = { { "prettierd", "prettier" } }
     },
-    {
-      cmd = { "isort", "-" }, -- 使用 isort 的 stdin/stdout 模式
-      filetypes = { "python" },
+    -- Set this to change the default values when calling conform.format()
+    -- This will also affect the default values for format_on_save/format_after_save
+    default_format_opts = {
+      lsp_format = "fallback",
     },
-
-    -- Java
-    {
-      cmd = { "google-java-format", "--replace", "-" }, -- 使用 google-java-format 的 stdin/stdout 模式
-      filetypes = { "java" },
+    -- If this is set, Conform will run the formatter on save.
+    -- It will pass the table to conform.format().
+    -- This can also be a function that returns the table.
+    format_on_save = {
+      -- I recommend these options. See :help conform.format for details.
+      lsp_format = "fallback",
+      timeout_ms = 500,
     },
-    -- 如果你使用 Maven 的 Spotless 插件，可以使用下面的配置
-    -- {
-    --   cmd = { "mvn", "com.diffplug.spotless:spotless-maven-plugin:apply" },
-    --   filetypes = { "java" },
-    -- },
-
-    -- JavaScript / TypeScript
-    {
-      cmd = { "prettier", "--stdin-filepath", "%:p" }, -- 使用 --stdin-filepath 来处理文件路径
-      filetypes = { "javascript", "typescript", "javascriptreact", "typescriptreact" },
+    -- If this is set, Conform will run the formatter asynchronously after save.
+    -- It will pass the table to conform.format().
+    -- This can also be a function that returns the table.
+    format_after_save = {
+      lsp_format = "fallback",
     },
-
-    -- Go
-    {
-      cmd = { "gofmt", "-w" },
-      filetypes = { "go" },
-    },
-
-    -- C/C++
-    {
-      cmd = { "clang-format" },
-      filetypes = { "c", "cpp", "objc", "objcpp" },
-    },
-
-    -- Rust
-    {
-      cmd = { "cargo", "fmt" },
-      filetypes = { "rust" },
-    },
-
-    -- JSON
-    {
-      cmd = { "jq", "." },
-      filetypes = { "json" },
-    },
-
-    -- Markdown
-    {
-      cmd = { "prettier", "--stdin-filepath", "%:p" },
-      filetypes = { "markdown" },
-    },
-
-    -- YAML
-    {
-      cmd = { "yq", "p", "-", "-" },
-      filetypes = { "yaml" },
+    -- Set the log level. Use `:ConformInfo` to see the location of the log file.
+    log_level = vim.log.levels.ERROR,
+    -- Conform will notify you when a formatter errors
+    notify_on_error = true,
+    -- Conform will notify you when no formatters are available for the buffer
+    notify_no_formatters = true,
+    -- Custom formatters and overrides for built-in formatters
+    formatters = {
+      my_formatter = {
+        -- This can be a string or a function that returns a string.
+        -- When defining a new formatter, this is the only field that is required
+        command = "my_cmd",
+        -- A list of strings, or a function that returns a list of strings
+        -- Return a single string instead of a list to run the command in a shell
+        args = { "--stdin-from-filename", "$FILENAME" },
+        -- If the formatter supports range formatting, create the range arguments here
+        range_args = function(self, ctx)
+          return { "--line-start", ctx.range.start[1], "--line-end", ctx.range["end"][1] }
+        end,
+        -- Send file contents to stdin, read new contents from stdout (default true)
+        -- When false, will create a temp file (will appear in "$FILENAME" args). The temp
+        -- file is assumed to be modified in-place by the format command.
+        stdin = true,
+        -- A function that calculates the directory to run the command in
+        cwd = require("conform.util").root_file({ ".editorconfig", "package.json" }),
+        -- When cwd is not found, don't run the formatter (default false)
+        require_cwd = true,
+        -- When stdin=false, use this template to generate the temporary file that gets formatted
+        tmpfile_format = ".conform.$RANDOM.$FILENAME",
+        -- When returns false, the formatter will not be used
+        condition = function(self, ctx)
+          return vim.fs.basename(ctx.filename) ~= "README.md"
+        end,
+        -- Exit codes that indicate success (default { 0 })
+        exit_codes = { 0, 1 },
+        -- Environment variables. This can also be a function that returns a table.
+        env = {
+          VAR = "value",
+        },
+        -- Set to false to disable merging the config with the base definition
+        inherit = true,
+        -- When inherit = true, add these additional arguments to the beginning of the command.
+        -- This can also be a function, like args
+        prepend_args = { "--use-tabs" },
+        -- When inherit = true, add these additional arguments to the end of the command.
+        -- This can also be a function, like args
+        append_args = { "--trailing-comma" },
+      },
+      -- These can also be a function that returns the formatter
+      other_formatter = function(bufnr)
+        return {
+          command = "my_cmd",
+        }
+      end,
     },
   }
-
-  conform.setup({
-    formatters = formatters,
-    format_on_save = {
-      timeout_ms = 500, -- 设置格式化延迟，以避免在保存文件时立即格式化，防止编辑器卡顿
-      post = { "m" }, -- 格式化后执行的命令，这里使用 'm' 来重新高亮显示
-    },
-    lsp_fallback = false, -- 如果 LSP 支持格式化，则优先使用 LSP，否则使用定义的格式化工具
-  })
-
-  -- 在 BufWritePre 事件中调用 conform.format_file() 来自动格式化文件
-  vim.api.nvim_create_autocmd({ "BufWritePre" }, {
-    callback = function()
-      local file_type = vim.bo.filetype
-      local formatter = conform.get_formatter_for_filetype(file_type)
-
-      if formatter then
-        conform.format_file({
-          async = true, -- 异步执行格式化
-          formatter = formatter,
-        })
-      end
-    end,
-  })
+  require("conform").setup(options)
+  -- You can set formatters_by_ft and formatters directly
+  require("conform").formatters_by_ft.lua = { "stylua" }
+  require("conform").formatters.my_formatter = {
+    command = "my_cmd",
+  }
 end
--- lua/modules/tool/package.lua
+++ lua/modules/tool/package.lua
@@ -5,6 +5,16 @@ return {
     event = { "BufWritePre" },
     cmd = { "ConformInfo" },
     config = require("modules.tool.conform"),
+    keys = {
+      {
+        "<leader>cF",
+        function()
+          require("conform").format({ formatters = { "injected" }, timeout_ms = 3000 })
+        end,
+        mode = { "n", "v" },
+        desc = "Format Injected Langs",
+      },
+    },
     init = function()
       -- If you want the formatexpr, here is the place to set it
       vim.o.formatexpr = "v:lua.require'conform'.formatexpr()"

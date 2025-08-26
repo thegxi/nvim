c714f322f08f352af1dab63575c5e23595f9467c
thegx <village.cowherd@gmail.com>
2025-08-27 00:55:01 +0800

    feat: 配置lewis6991/gitsigns.nvim

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,5 +1,6 @@
 return {
 	{ import = "plugins.ui.colorscheme" },
 	{ import = "plugins.ui.treesitter" },
+	{ import = "plugins.ui.gitsigns" },
 	{ import = "plugins.tools.snacks" },
 }
--- lua/plugins/tools/snacks.lua
+++ lua/plugins/tools/snacks.lua
@@ -21,7 +21,16 @@
         quickfile = { enabled = true },
         scope = { enabled = true },
         scroll = { enabled = true },
-        statuscolumn = { enabled = true },
+        statuscolumn = { 
+          enabled = true, 
+          left = { "mark", "sign" }, -- priority of signs on the left (high to low)
+          right = { "fold", "git" }, -- priority of signs on the right (high to low)
+          folds = {
+            open = true, -- show open fold icons
+            git_hl = false, -- use Git Signs hl for fold icons
+          },
+          refresh = 50, -- refresh at most every 50ms
+        },
         words = { enabled = true },
         styles = {
           notification = {

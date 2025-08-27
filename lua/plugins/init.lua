0f424b37544435a343bacbda58cfd324d1e3e008
thegx <village.cowherd@gmail.com>
2025-08-27 21:14:10 +0800

    feat: 配置nvim-lualine/lualine.nvim

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -2,5 +2,6 @@ return {
 	{ import = "plugins.ui.colorscheme" },
 	{ import = "plugins.ui.treesitter" },
 	{ import = "plugins.ui.gitsigns" },
+	{ import = "plugins.ui.lualine" },
 	{ import = "plugins.tools.snacks" },
 }

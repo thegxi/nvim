f6f1ee22e979002ffc911a010ad43839fdbba093
thegx <village.cowherd@gmail.com>
2025-08-27 21:19:36 +0800

    feat: 配置nvim-tree/nvim-web-devicons

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -3,5 +3,6 @@ return {
 	{ import = "plugins.ui.treesitter" },
 	{ import = "plugins.ui.gitsigns" },
 	{ import = "plugins.ui.lualine" },
+	{ import = "plugins.ui.nvim-web-devicons" },
 	{ import = "plugins.tools.snacks" },
 }

2698799468d9e311d40d0cc0b4ccbf7c98ee4361
thegx <village.cowherd@gmail.com>
2025-08-27 22:32:59 +0800

    feat: 配置numToStr/Comment.nvim

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -87,5 +87,21 @@ return {
     opts={
         --Config goes here
     },
-  }
+  },
+  ------------------------------ Code comment ------------------------------ 
+  {
+    "numToStr/Comment.nvim",
+    -- stylua: ignore
+    keys = {
+      { "<leader>/", function() require("Comment.api").toggle.linewise.current() end,                 mode = "n", desc = "[Comment] Comment current line", },
+      { "<leader>/", "<esc><cmd>lua require('Comment.api').toggle.linewise(vim.fn.visualmode())<cr>", mode = "v", desc = "Comment current line",           },
+      -- control + / keymappings
+      { "<C-_>",     function() require("Comment.api").toggle.linewise.current() end,                 mode = "n", desc = "[Comment] Comment current line", },
+      { "<C-_>",     "<esc><cmd>lua require('Comment.api').toggle.linewise(vim.fn.visualmode())<cr>", mode = "v", desc = "Comment current line",           },
+
+    },
+    config = function()
+      require("Comment").setup()
+    end,
+  },
 }

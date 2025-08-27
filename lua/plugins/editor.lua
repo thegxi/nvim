a782c19d055d5757aa31c8f1dd50ff3d1777e593
thegx <village.cowherd@gmail.com>
2025-08-27 22:34:52 +0800

    feat: 配置folke/todo-comments.nvim

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -104,4 +104,18 @@ return {
       require("Comment").setup()
     end,
   },
+  ------------------------------  highlight and search for todo comments like TODO, HACK, BUG in your code base ------------------------------ 
+  {
+    "folke/todo-comments.nvim",
+    dependencies = { "nvim-lua/plenary.nvim", "folke/snacks.nvim" },
+    event = "BufReadPost",
+    -- stylua: ignore
+    keys = {
+      ---@diagnostic disable-next-line: undefined-field
+      { "<leader>st", function() require("snacks").picker.todo_comments({ keywords = { "TODO", "FIX", "FIXME", "BUG", "FIXIT", "HACK", "WARN", "ISSUE"  } }) end, desc = "[TODO] Pick todos (without NOTE)", },
+      ---@diagnostic disable-next-line: undefined-field
+      { "<leader>sT", function() require("snacks").picker.todo_comments() end, desc = "[TODO] Pick todos (with NOTE)", },
+    },
+    config = true,
+  },
 }

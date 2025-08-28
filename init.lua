eda7a671c577805d5b4c080fb0b3f046afd18d6b
thegx <village.cowherd@gmail.com>
2025-08-28 21:01:31 +0800

    feat: 配置gui(neovide)

--- init.lua
+++ init.lua
@@ -1,6 +1,10 @@
 vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
 
+if vim.g.neovide then
+  require("neovide.neovide")
+end
+
 require 'config.options'
 require 'config.lazy'
 require 'config.keymapping'

bd3294a80633f4a8cd19a7a2384d4c1a2c874d1e
thegx <village.cowherd@gmail.com>
2025-08-28 21:10:52 +0800

    style: 去除重复的快捷键设置

--- lua/plugins/tools.lua
+++ lua/plugins/tools.lua
@@ -2,12 +2,6 @@ return {
   {
     "mikavilpas/yazi.nvim",
     dependencies = { "folke/snacks.nvim" },
-    -- stylua: ignore
-    keys = {
-      { "<leader>E",  "<CMD>Yazi<CR>",        desc = "[Yazi] open at the current file", mode = { "n", "v" }},
-      { "<leader>cw", "<CMD>Yazi cwd<CR>",    desc = "[Yazi] open in working directory"             },
-      { "<c-up>",     "<CMD>Yazi toggle<CR>", desc = "[Yazi] Resume the last session"                      },
-    },
     ---@type YaziConfig | {}
     opts = {
       -- if you want to open yazi instead of netrw, see below for more info

c874c41d9e895a6e6a8e9c862dc8b15c80a9f910
xi <village.cowherd@gmail.com>
2025-12-13 00:10:03 +0800

    update: lsp基本配置

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -3,20 +3,37 @@ return {
     "mason-org/mason.nvim",
     opts = {
       ensure_installed = {
-	"lua-language-server"
+        "lua-language-server",
       },
       ui = {
         icons = {
           package_installed = "✓",
           package_pending = "➜",
-          package_uninstalled = "✗"
-        }
-      }
+          package_uninstalled = "✗",
+        },
+      },
     },
     config = function(_, opts)
       require("mason").setup(opts)
       local mr = require("mason-registry")
-      
-    end
-  }
+
+      local function ensure_installed()
+        for _, tool in ipairs(opts.ensure_installed) do
+          if mr.has_package(tool) then
+            local p = mr.get_package(tool)
+            if not p:is_installed() then
+              p:install()
+            end
+          end
+        end
+      end
+
+      if mr.refresh then
+        mr.refresh(ensure_installed)
+      else
+        ensure_installed()
+      end
+    end,
+  },
 }
+

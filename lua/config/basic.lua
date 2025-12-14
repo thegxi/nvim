8b4034b3965eb52bbeac27626d7312e453d69d29
xi <village.cowherd@gmail.com>
2025-12-14 17:46:51 +0800

    update: 显示行号

--- lua/config/basic.lua
+++ lua/config/basic.lua
@@ -1,10 +1,15 @@
 local opt = vim.opt
 
+opt.number = true
+opt.cusorline = true
+
+opt.list = true
+opt.listchars = { tab = ">-", trail = "-" }
+
 opt.tabstop = 2
 opt.shiftwidth = 2
 opt.softtabstop = 2
 opt.expandtab = true
+opt.smartindent = true
 
-opt.list = true
-opt.listchars = { tab = ">-", trail = "-" }
 

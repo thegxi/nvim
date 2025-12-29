828874d6b1171b037f3448e95b63f758403e2de8
xi <village.cowherd@gmail.com>
2025-12-29 22:36:31 +0800

    feat: 数据库操作按键映射

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,1 +1,7 @@
+-- 打开数据库UI界面
+vim.keymap.set("n", "<leader>du", "<CMD>DBUI<CR>", { desc = "打开数据库 UI" })
+
+-- 执行当前行或选中区的sql
+vim.keymap.set("n", "<leader>ds", ":DB<CR>", { desc = "执行sql" })
+vim.keymap.set("v", "<leader>ds", ":'<,'>DB<CR>", { desc = "执行选中sql" })
 

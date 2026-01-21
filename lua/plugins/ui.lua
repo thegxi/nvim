5f3d86b52bb0513e224778664e7cfe4a8eba135e
xi <village.cowherd@gmail.com>
2026-01-21 20:29:15 +0800

    update: 重新配置插件

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -1,11 +1,16 @@
-return  {
-  "sainnhe/gruvbox-material",
-  lazy = false,
-  priority = 1000,
-  config = function()
-    -- Optionally configure and load the colorscheme
-    -- directly inside the plugin declaration.
-    vim.g.gruvbox_material_enable_italic = true
-    vim.cmd.colorscheme('gruvbox-material')
-  end
+return {
+  {
+    "sainnhe/gruvbox-material",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      -- Optionally configure and load the colorscheme
+      -- directly inside the plugin declaration.
+      vim.g.gruvbox_material_enable_italic = true
+      -- vim.g.gruvbox_material_foreground = "material"
+      vim.g.gruvbox_material_foreground = "mix"
+      -- vim.g.gruvbox_material_foreground = "original"
+      vim.cmd.colorscheme('gruvbox-material')
+    end
+  }
 }

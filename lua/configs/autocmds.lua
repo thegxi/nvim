1fa9daf8095d15b559338ae5eb5c5bb9e668aa58
xi <village.cowherd@gmail.com>
2026-05-18 23:02:14 +0800

    update: 自定义lazyFile事件

--- lua/configs/autocmds.lua
+++ lua/configs/autocmds.lua
@@ -5,3 +5,27 @@ vim.api.nvim_create_autocmd({ "VimEnter" }, {
     end)
   end
 })
+
+-- 自定义懒加载事件
+vim.api.nvim_create_autocmd({ "BufReadPost", "BufNewFile" }, {
+  desc = "自定义事件LazyFile",
+  pattern = "*",
+  once = true,
+  callback = function()
+    if not vim.g._lazyfile_triggered then
+      vim.g._lazyfile_triggered = true
+      vim.schedule(function()
+        vim.api.nvim_exec_autocmds("User", { pattern = "LazyFile" })
+      end)
+    end
+  end,
+})
+
+-- 取消自动换行注释
+vim.api.nvim_create_autocmd("FileType", {
+  desc = "取消自动换行注释",
+  pattern = "*",
+  callback = function()
+    vim.opt_local.formatoptions:remove({ "c", "r" })
+  end,
+})
--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -2,8 +2,24 @@ local map = vim.keymap.set
 
 -- devicons
 vim.api.nvim_create_autocmd("User", {
-  pattern = "CookLazy",
-  callback = function()
-    require("nvim-web-devicons")
-  end
+	pattern = "CookLazy",
+	callback = function()
+		require("nvim-web-devicons")
+	end,
+})
+
+--bufferline
+vim.api.nvim_create_autocmd("User", {
+	pattern = "LazyFile",
+	callback = function()
+		require("bufferline").setup({})
+		map("n", "]b", "<cmd>BufferLineCycleNext<cr>", { desc = "切换到下一个缓冲区" })
+		map("n", "[b", "<cmd>BufferLineCyclePrev<cr>", { desc = "切换到上一个缓冲区" })
+		map("n", "<leader>bb", "<cmd>e #<cr>", { desc = "快速切换缓冲区" })
+		map("n", "<leader>bd", "<cmd>bdelete<cr>", { desc = "删除缓冲区" })
+		map("n", "<leader>bf", "<cmd>BufferLinePick<cr>", { desc = "查询并跳转缓冲区" })
+		map("n", "<leader>bo", "<cmd>BufferLineCloseOthers<cr>", { desc = "删除其他缓冲区" })
+		map("n", "<leader>bp", "<cmd>BufferLineTogglePin<cr>", { desc = "切换缓冲区固定状态" })
+		map("n", "<leader>bP", "<cmd>BufferLineGroupClose ungrouped<cr>", { desc = "删除未固定的缓冲区" })
+	end,
 })

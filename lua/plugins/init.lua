4651a80642cb56d4e98337d16dcea4ab374dcd80
xi <village.cowherd@gmail.com>
2026-05-19 20:58:36 +0800

    feat: 增加ai插件

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -3,35 +3,38 @@ local M = {}
 local install_list = {}
 
 M.plugins_list = {
-  editor = {
-    { src = "https://github.com/stevearc/oil.nvim" }, -- 文件管理
-  },
-  lsp = {
-    { src = "https://github.com/neovim/nvim-lspconfig" }, -- lsp配置
-    { src = "https://github.com/mason-org/mason.nvim" }, -- lsp安装
-    { src = "https://github.com/stevearc/conform.nvim" }, -- 格式化配置
-  },
-  ui = {
-    { src = "https://github.com/folke/tokyonight.nvim" }, -- 主题配色
-    { src = "https://github.com/catppuccin/nvim" }, -- 主题配色
-    { src = "https://github.com/nvim-tree/nvim-web-devicons" }, -- 文件图标
-    { src = "https://github.com/akinsho/bufferline.nvim", tag = "*", requires = "nvim-tree/nvim-web-devicons" }     -- bufferline
-  },
-  utils = {
-    { src = "https://github.com/windwp/nvim-autopairs" },  -- 括号成对
-    { src = "https://github.com/ibhagwan/fzf-lua" },       -- 搜索
-    { src = "https://github.com/folke/flash.nvim" }, -- 跳转
-    { src = "https://github.com/HiPhish/rainbow-delimiters.nvim" }, -- 彩虹括号
-    { src = "https://github.com/DrKJeff16/project.nvim" }, -- 项目跳转
-    { src = "https://github.com/kylechui/nvim-surround", version = vim.version.range("4.x") }, -- 快捷添加括号与引号
-    { src = "https://github.com/folke/which-key.nvim" },   -- 快捷键显示
-    { src = "https://github.com/Youthdreamer/obsess" },    -- 专注任务面板
-    { src = "https://github.com/kdheepak/lazygit.nvim", requires = "nvim-lua/plenary.nvim" } -- git
-  }
+	editor = {
+		{ src = "https://github.com/stevearc/oil.nvim" }, -- 文件管理
+	},
+	lsp = {
+		{ src = "https://github.com/neovim/nvim-lspconfig" }, -- lsp配置
+		{ src = "https://github.com/mason-org/mason.nvim" }, -- lsp安装
+		{ src = "https://github.com/stevearc/conform.nvim" }, -- 格式化配置
+	},
+	ui = {
+		{ src = "https://github.com/folke/tokyonight.nvim" }, -- 主题配色
+		{ src = "https://github.com/catppuccin/nvim" }, -- 主题配色
+		{ src = "https://github.com/nvim-tree/nvim-web-devicons" }, -- 文件图标
+		{ src = "https://github.com/akinsho/bufferline.nvim", tag = "*", requires = "nvim-tree/nvim-web-devicons" }, -- bufferline
+	},
+	utils = {
+		{ src = "https://github.com/windwp/nvim-autopairs" }, -- 括号成对
+		{ src = "https://github.com/ibhagwan/fzf-lua" }, -- 搜索
+		{ src = "https://github.com/folke/flash.nvim" }, -- 跳转
+		{ src = "https://github.com/HiPhish/rainbow-delimiters.nvim" }, -- 彩虹括号
+		{ src = "https://github.com/DrKJeff16/project.nvim" }, -- 项目跳转
+		{ src = "https://github.com/kylechui/nvim-surround", version = vim.version.range("4.x") }, -- 快捷添加括号与引号
+		{ src = "https://github.com/folke/which-key.nvim" }, -- 快捷键显示
+		{ src = "https://github.com/Youthdreamer/obsess" }, -- 专注任务面板
+		{ src = "https://github.com/kdheepak/lazygit.nvim" }, -- git
+		{ src = "https://github.com/olimorris/codecompanion.nvim" }, -- ai
+		{ src = "https://github.com/nvim-lua/plenary.nvim" }, -- 公共依赖
+		{ src = "https://github.com/nvim-treesitter/nvim-treesitter" }, -- 语法高亮
+	},
 }
 
 for _, group in pairs(M.plugins_list) do
-  vim.list_extend(install_list, group)
+	vim.list_extend(install_list, group)
 end
 
 vim.pack.add(install_list, { load = false })
--- lua/plugins/utils.lua
+++ lua/plugins/utils.lua
@@ -2,127 +2,132 @@ local map = vim.keymap.set
 
 vim.api.nvim_create_autocmd("InsertEnter", {
 
-  once = true,
-  callback = function()
-    -- autopairs
-    require("nvim-autopairs").setup()
-    -- surround
-    require("nvim-surround").setup({
-        --     Old text                    Command         New text
-        -- --------------------------------------------------------------------------------
-        --     surr*ound_words             ysiw)           (surround_words)
-        --     surr*ound_words             ysiw(           ( surround_words )
-        --     *make strings               ys$"            "make strings"
-        --     [delete ar*ound me!]        ds]             delete around me!
-        --     remove <b>HTML t*ags</b>    dst             remove HTML tags
-        --     'change quot*es'            cs'"            "change quotes"
-        --     <b>or tag* types</b>        csth1<CR>       <h1>or tag types</h1>
-        --     delete(functi*on calls)     dsf             function calls
-    })
-    -- rainbow-delimiters
-    require('rainbow-delimiters.setup').setup{}
-  end,
+	once = true,
+	callback = function()
+		-- autopairs
+		require("nvim-autopairs").setup()
+		-- surround
+		require("nvim-surround").setup({
+			--     Old text                    Command         New text
+			-- --------------------------------------------------------------------------------
+			--     surr*ound_words             ysiw)           (surround_words)
+			--     surr*ound_words             ysiw(           ( surround_words )
+			--     *make strings               ys$"            "make strings"
+			--     [delete ar*ound me!]        ds]             delete around me!
+			--     remove <b>HTML t*ags</b>    dst             remove HTML tags
+			--     'change quot*es'            cs'"            "change quotes"
+			--     <b>or tag* types</b>        csth1<CR>       <h1>or tag types</h1>
+			--     delete(functi*on calls)     dsf             function calls
+		})
+		-- rainbow-delimiters
+		require("rainbow-delimiters.setup").setup({})
+	end,
 })
 
 vim.api.nvim_create_autocmd("User", {
-  pattern = "CookLazy",
-  callback = function()
-    -- fzflua
-    require("fzf-lua").setup()
-    map("n", "<leader>ff", "<cmd>FzfLua files<cr>", { desc = "查找文件" })
-    map("n", "<leader>fg", "<cmd>FzfLua live_grep<cr>", { desc = "全局查找" })
-    map("n", "<leader>fb", "<cmd>FzfLua buffers<cr>", { desc = "缓冲区查找" })
-    map("n", "<leader>fo", "<cmd>FzfLua oldfiles<cr>", { desc = "最近使用文件查找" })
-    map("n", "<leader>fs", "<cmd>FzfLua grep_cword<cr>", { desc = "当前单词查找" })
-    map("n", "<leader>fk", "<cmd>FzfLua keymap<cr>", { desc = "查找快捷键" })
-    map("n", "<leader>fh", "<cmd>FzfLua helptags<cr>", { desc = "查找快捷键" })
-    map("n", "<leader>cR", function()
-      vim.lsp.buf.rename()
-    end, { desc = "LSP Rename" })
-    map("n", "<leader>cd", "<cmd>FzfLua lsp_definitions<cr>", { desc = "跳转定义" })
-    map("n", "<leader>cr", "<cmd>FzfLua lsp_references<cr>", { desc = "引用" })
-    map("n", "<leader>ci", "<cmd>FzfLua lsp_implementations<cr>", { desc = "实现" })
-    map("n", "<leader>ct", "<cmd>FzfLua lsp_typedefs<cr>", { desc = "类型定义" })
-    map("n", "<leader>ca", "<cmd>FzfLua lsp_code_actions<cr>", { desc = "代码操作" })
-    map("n", "<leader>cs", "<cmd>FzfLua lsp_document_symbols<cr>", { desc = "当前文件符号" })
-    map("n", "<leader>cw", "<cmd>FzfLua lsp_workspace_symbols<cr>", { desc = "当前项目符号" })
-    map("n", "<leader>cW", "<cmd>FzfLua diagnostics_workspace<cr>", { desc = "全局诊断" })
-    map("n", "<leader>lw", "<cmd>FzfLua lsp_workspace_symbols<cr>", { desc = "当前项目符号" })
+	pattern = "CookLazy",
+	callback = function()
+		-- fzflua
+		require("fzf-lua").setup()
+		map("n", "<leader>ff", "<cmd>FzfLua files<cr>", { desc = "查找文件" })
+		map("n", "<leader>fg", "<cmd>FzfLua live_grep<cr>", { desc = "全局查找" })
+		map("n", "<leader>fb", "<cmd>FzfLua buffers<cr>", { desc = "缓冲区查找" })
+		map("n", "<leader>fo", "<cmd>FzfLua oldfiles<cr>", { desc = "最近使用文件查找" })
+		map("n", "<leader>fs", "<cmd>FzfLua grep_cword<cr>", { desc = "当前单词查找" })
+		map("n", "<leader>fk", "<cmd>FzfLua keymap<cr>", { desc = "查找快捷键" })
+		map("n", "<leader>fh", "<cmd>FzfLua helptags<cr>", { desc = "查找快捷键" })
+		map("n", "<leader>cR", function()
+			vim.lsp.buf.rename()
+		end, { desc = "LSP Rename" })
+		map("n", "<leader>cd", "<cmd>FzfLua lsp_definitions<cr>", { desc = "跳转定义" })
+		map("n", "<leader>cr", "<cmd>FzfLua lsp_references<cr>", { desc = "引用" })
+		map("n", "<leader>ci", "<cmd>FzfLua lsp_implementations<cr>", { desc = "实现" })
+		map("n", "<leader>ct", "<cmd>FzfLua lsp_typedefs<cr>", { desc = "类型定义" })
+		map("n", "<leader>ca", "<cmd>FzfLua lsp_code_actions<cr>", { desc = "代码操作" })
+		map("n", "<leader>cs", "<cmd>FzfLua lsp_document_symbols<cr>", { desc = "当前文件符号" })
+		map("n", "<leader>cw", "<cmd>FzfLua lsp_workspace_symbols<cr>", { desc = "当前项目符号" })
+		map("n", "<leader>cW", "<cmd>FzfLua diagnostics_workspace<cr>", { desc = "全局诊断" })
+		map("n", "<leader>lw", "<cmd>FzfLua lsp_workspace_symbols<cr>", { desc = "当前项目符号" })
 
-    -- project
-    require("project").setup({
-      fzf_lua = {
-        enabled = true,
-      },
-      ignore_lsp = true,
-    })
-    vim.keymap.set("n", "<leader>fp", "<cmd>Project<cr>")
+		-- project
+		require("project").setup({
+			fzf_lua = {
+				enabled = true,
+			},
+			ignore_lsp = true,
+		})
+		vim.keymap.set("n", "<leader>fp", "<cmd>Project<cr>")
 
-    -- which-key
-    local wk = require("which-key")
-    local opts = {
-      preset = "helix",
-    }
-    wk.setup(opts)
-    wk.add({
-      { "<leader>e", group = " Oil", icon = "" },
-      { "<leader>b", group = "缓冲区操作", icon = "" },
-      { "<leader>f", group = "文件查找", icon = "󰈞" },
-      { "<leader>c", group = "LSP操作", icon = "" },
-      { "<leader>q", group = "退出编辑器", icon = "󰩈" },
-      { "<leader>w", group = "窗口操作", icon = "" },
-      { "<leader><tab>", group = "标签栏", icon = "󰓩" },
-      { "<leader>o", group = "专注面板", icon = "" },
-      { "<leader>u", group = "Undotree", icon = "󰕌" },
-      { "<leader>l", group = "项目相关" },
-    })
+		-- which-key
+		local wk = require("which-key")
+		local opts = {
+			preset = "helix",
+		}
+		wk.setup(opts)
+		wk.add({
+			{ "<leader>e", group = " Oil", icon = "" },
+			{ "<leader>b", group = "缓冲区操作", icon = "" },
+			{ "<leader>f", group = "文件查找", icon = "󰈞" },
+			{ "<leader>c", group = "LSP操作", icon = "" },
+			{ "<leader>q", group = "退出编辑器", icon = "󰩈" },
+			{ "<leader>w", group = "窗口操作", icon = "" },
+			{ "<leader><tab>", group = "标签栏", icon = "󰓩" },
+			{ "<leader>o", group = "专注面板", icon = "" },
+			{ "<leader>u", group = "Undotree", icon = "󰕌" },
+			{ "<leader>l", group = "项目相关" },
+		})
 
-    -- obsess
-    require("obsess").setup({
-      position = "center",
-      window = {
-        width  = 60,
-        height = 15,
-        title  = "Obsess",
-      },
-      -- 倒计时结束后的弹窗提醒设置
-      flash = {
-        times = 15,        -- 闪烁次数
-        interval_ms = 200, -- 每次间隔时间
-      }
-    })
-    map("n", "<leader>os", "<cmd>ObsessToggle<CR>", { desc = "切换窗口" })
-    map("n", "<leader>oc", "<cmd>ObsessClose<CR>", { desc = "注销" })
-    map("n", "<leader>oo", "<cmd>ObsessTimer<CR>", { desc = "设置定时器(m)" })
-    map("n", "<leader>ol", "<cmd>ObsessTimerSec<CR>", { desc = "设置定时器(s)" })
-    map("n", "<leader>oa", "<cmd>ObsessTaskAdd<CR>", { desc = "添加任务" })
-    map("n", "<leader>ot", "<cmd>ObsessTaskDone<CR>", { desc = "切换任务状态" })
-    map("n", "<leader>od", "<cmd>ObsessTaskDel<CR>", { desc = "删除任务" })
-    map("n", "<leader>oe", "<cmd>ObsessTaskClear<CR>", { desc = "清空任务列表" })
-  end,
+		-- obsess
+		require("obsess").setup({
+			position = "center",
+			window = {
+				width = 60,
+				height = 15,
+				title = "Obsess",
+			},
+			-- 倒计时结束后的弹窗提醒设置
+			flash = {
+				times = 15, -- 闪烁次数
+				interval_ms = 200, -- 每次间隔时间
+			},
+		})
+		map("n", "<leader>os", "<cmd>ObsessToggle<CR>", { desc = "切换窗口" })
+		map("n", "<leader>oc", "<cmd>ObsessClose<CR>", { desc = "注销" })
+		map("n", "<leader>oo", "<cmd>ObsessTimer<CR>", { desc = "设置定时器(m)" })
+		map("n", "<leader>ol", "<cmd>ObsessTimerSec<CR>", { desc = "设置定时器(s)" })
+		map("n", "<leader>oa", "<cmd>ObsessTaskAdd<CR>", { desc = "添加任务" })
+		map("n", "<leader>ot", "<cmd>ObsessTaskDone<CR>", { desc = "切换任务状态" })
+		map("n", "<leader>od", "<cmd>ObsessTaskDel<CR>", { desc = "删除任务" })
+		map("n", "<leader>oe", "<cmd>ObsessTaskClear<CR>", { desc = "清空任务列表" })
+	end,
 })
 
 -- flash
-map({"n", "x", "o"}, "s", function()
-  local flash = require("flash")
-  flash.jump()
+map({ "n", "x", "o" }, "s", function()
+	local flash = require("flash")
+	flash.jump()
 end)
-map({"n", "x", "o"}, "S", function()
-  local flash = require("flash")
-  flash.treesitter()
+map({ "n", "x", "o" }, "S", function()
+	local flash = require("flash")
+	flash.treesitter()
 end)
 map("o", "r", function()
-  local flash = require("flash")
-  flash.remote()
+	local flash = require("flash")
+	flash.remote()
 end)
-map({"x", "o"}, "R", function()
-  local flash = require("flash")
-  flash.treesitter_search()
+map({ "x", "o" }, "R", function()
+	local flash = require("flash")
+	flash.treesitter_search()
 end)
 map("c", "<a-s>", function()
-  local flash = require("flash")
-  flash.toggle()
+	local flash = require("flash")
+	flash.toggle()
 end)
 
 -- lazygit
 map("n", "<leader>gg", "<cmd>LazyGit<cr>", { desc = "LazyGit" })
+
+-- codecompanion
+require("codecompanion").setup({
+	log_level = "DEBUG",
+})

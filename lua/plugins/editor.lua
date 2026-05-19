b88496f71750c7c0c453b081fcc61fc487c52ae7
xi <village.cowherd@gmail.com>
2026-05-19 22:04:06 +0800

    style: 格式化

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -1,24 +1,23 @@
 -- Oil
 require("oil").setup({
-    columns = {
-        "icon"
-    },
-    win_options = {
-        cursorline = true,
-        signcolumn = "no",
-        wrap = false,
-        list = false,
-        conceallevel = 3
-    }
+	columns = {
+		"icon",
+	},
+	win_options = {
+		cursorline = true,
+		signcolumn = "no",
+		wrap = false,
+		list = false,
+		conceallevel = 3,
+	},
 })
 local function toggle_oil()
-  local bufname = vim.api.nvim_buf_get_name(0)
-  if vim.bo.filetype == "oil" then
-    require("oil").close()
-  else
-    require("oil").open()
-  end
+	if vim.bo.filetype == "oil" then
+		require("oil").close()
+	else
+		require("oil").open()
+	end
 end
 vim.keymap.set("n", "<leader>e", function()
-  toggle_oil()
+	toggle_oil()
 end, { desc = "Toggle Oil" })
